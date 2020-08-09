39ec16538d516fb56779fb97a64719abcc69951f
Charlie Gooderham <charlie@euphoriq.co.uk>
2020-08-09 18:34:30 +0100

    Dynamically render the correct number of burger ingredients dependent on state.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    /* Create an array containing an array per key in 'ingredients' that contains the appropriate number of burger
+    ingredients */
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            })
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="salad" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
