b77b0c27ee2a45840e491933df7001e6c94abc99
Charlie Gooderham <charlie@euphoriq.co.uk>
2020-08-09 18:47:38 +0100

    Amend logic to add a fallback/no ingredients message if a user hasn't added any ingredients yet.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     /* Create an array containing an array per key in 'ingredients' that contains the appropriate number of burger
-    ingredients */
-    const transformedIngredients = Object.keys(props.ingredients)
+    ingredient objects i.e. [[{...}, {...}], [{...}], [{...}, {...}]]. '.reduce()' Then flattens this down so that all
+    of the sub array values are concatenated to a new array (so we can easily check length). */
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             })
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients to your burger!</p>;
+    }
 
     return (
         <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            cheese: 2,
-            meat: 3,
-            salad: 1,
-            bacon: 1
+            cheese: 0,
+            meat: 0,
+            salad: 0,
+            bacon: 0
         }
     }
 
