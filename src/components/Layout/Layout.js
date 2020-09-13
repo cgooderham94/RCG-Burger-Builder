5e905ec8f0dbe37df452434df6662bca831eeeb4
Charlie Gooderham <charlie@euphoriq.co.uk>
2020-09-13 16:12:43 +0100

    Add side drawer menu toggle button and use prevState to toggle the state based on the current state.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,11 +7,17 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
     }
 
     render() {
@@ -19,7 +25,7 @@ class Layout extends Component {
             <Aux>
                 <div>Toolbar, SideDrawer, Backdrop</div>
 
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
 
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
 
