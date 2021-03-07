e986a46439ff9f6de67c1980d4908d43a9858b8c
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-03-07 19:14:43 +0000

    Add start, success, fail and auth (async) auth actions (WIP). Dispatch auth action inside auth container upon click of login form submit button.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
     state = {
@@ -86,6 +88,12 @@ class Auth extends Component {
         return isValid;
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render () {
         const formElementsArray = [];
 
@@ -110,14 +118,20 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
 
-                    <Button btnType="Success" >Log In</Button>
+                    <Button btnType="Success">Log In</Button>
                 </form>
             </div>
         );
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
