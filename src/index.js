e6bdd023af5847974b5aa2131c5948c838def66b
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-03-10 19:09:39 +0000

    Add auth reducer to manage user auth state including token, id, errors and loading.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import authReducer from './store/reducers/auth';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
-import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/order';
+import thunk from 'redux-thunk';
 
 const logger = store => {
   return next => {
@@ -24,6 +25,7 @@ const logger = store => {
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
+  auth: authReducer,
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer
 });
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,48 @@
+import * as actionTypes from './../actions/actionTypes';
+import { updateObj } from '../utility';
+import { bindActionCreators } from 'redux';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+}
+
+const authStart = (state) => {
+    return updateObj(state, {
+        error: null,
+        loading: true
+    });
+};
+
+const authSuccess = (state, action) => {
+    return updateObj(state, {
+        token: action.userToken,
+        userId: action.userId,
+        error: null,
+        loading: false
+    });
+};
+
+const authFail = (state, action) => {
+    return updateObj(state, {
+        error: action.error,
+        loading: false
+    });
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.AUTH_START:
+            return authStart(state)
+        case actionTypes.AUTH_SUCCESS:
+            return authSuccess(state, action);
+        case actionTypes.AUTH_START:
+            return authFail(state, action)
+        default:
+            return state;
+    }
+}
+
+export default reducer;
